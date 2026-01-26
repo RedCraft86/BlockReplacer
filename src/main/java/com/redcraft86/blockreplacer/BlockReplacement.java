b24f3a400bc40ffb71dab9c34b274f0401fa8a38
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-01-26 16:17:34 -0500

    Record over class

--- src/main/java/com/redcraft86/blockreplacer/BlockReplacement.java
+++ src/main/java/com/redcraft86/blockreplacer/BlockReplacement.java
@@ -1,38 +1,27 @@
 package com.redcraft86.blockreplacer;
 
+import java.util.Map;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 
-public final class BlockReplacement {
-    private final BlockState newState;
-    private Int2ObjectOpenHashMap<Property<?>> newProps;
-
-    public BlockReplacement(Block newBlock) {
-        newState = newBlock.defaultBlockState();
-        newProps = null;
-    }
-
-    public BlockState getNewState() {
-        return newState;
-    }
-
-    public Block getNewBlock() {
-        return newState.getBlock();
-    }
+public record BlockReplacement(Block newBlock) {
+    private static final Map<Block, Int2ObjectOpenHashMap<Property<?>>> PROPERTY_CACHE = new Reference2ReferenceOpenHashMap<>();
 
     @SuppressWarnings("unchecked")
     public BlockState replace(BlockState oldState) {
-        if (newProps == null) {
-            newProps = new Int2ObjectOpenHashMap<>();
-            for (Property<?> property : newState.getProperties()) {
-                newProps.put(property.generateHashCode(), property);
+        Int2ObjectOpenHashMap<Property<?>> newProps = PROPERTY_CACHE.computeIfAbsent(newBlock, k -> {
+            Int2ObjectOpenHashMap<Property<?>> properties = new Int2ObjectOpenHashMap<>();
+            for (Property<?> property : k.defaultBlockState().getProperties()) {
+                properties.put(property.generateHashCode(), property);
             }
-        }
+            return properties;
+        });
 
-        BlockState mergedState = newState;
+        BlockState mergedState = newBlock.defaultBlockState();
         for (Property<?> property : oldState.getProperties()) {
             //noinspection rawtypes
             Property prop = newProps.get(property.generateHashCode());
